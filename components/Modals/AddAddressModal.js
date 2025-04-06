af6f219fc0d74f69c1fb6c778e69e5829c06fad6
amin-rayati <aminrayati469@gmail.com>
2025-04-06 23:00:01 +0330

    fix: change شییقثسئخیشم

--- components/Modals/AddAddressModal.js
+++ components/Modals/AddAddressModal.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { motion } from "framer-motion";
 import { commonAnimations } from "@/utils/animations";
